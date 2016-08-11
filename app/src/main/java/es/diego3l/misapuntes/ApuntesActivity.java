c02f94b5c3d13cf0ae3f917f5911a26e02560da4
balay1 <dlolhd@gmail.com>
2016-08-11 12:06:50 +0200

    Añadidos nuevos Items de Nuevos Apuntes y Salir de la aplicación

--- app/src/main/java/es/diego3l/misapuntes/ApuntesActivity.java
+++ app/src/main/java/es/diego3l/misapuntes/ApuntesActivity.java
@@ -5,11 +5,13 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 public class ApuntesActivity extends AppCompatActivity {
     private ListView listView;
@@ -41,16 +43,20 @@ public class ApuntesActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
+
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch (id){
+            case R.id.action_nuevo:
+                //crear nuevo aviso
+                Log.d(getLocalClassName(), "Crear Nuevo Apunte");
+                Toast.makeText( getApplicationContext(), "Has pulsado en Nuevo Apunte", Toast.LENGTH_SHORT ).show();
+                return true;
+            case R.id.action_salir:
+                finish();
+                return true;
+            default:
+                return false;
         }
-
-        return super.onOptionsItemSelected(item);
     }
 }
